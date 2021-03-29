46d0839acf2a57788ee3aa8de5b62898fd5b9900
AtomicTempest <atomictempest@tutamail.com>
2021-03-29 13:31:58 +1300

    added requirements.txt for setting up virtual enviroment and installing Discord.py. Added .gitignore to ignore venv folders and pyCharms .idea folder

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import discord
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -13,5 +15,6 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
 
 client.run('your token here')

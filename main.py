ab7673ad46537fd5ca202551ab213cf8b632a1ae
AtomicTempest <atomictempest@tutamail.com>
2021-04-01 11:05:12 +1300

    Added fuction to check discord messages for commands and call relevant Added function to retrieve data given parameters in a discord message and reply with it.

--- main.py
+++ main.py
@@ -12,8 +12,38 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import discord
+from sql import create_table, retrieve_data
 
 client = discord.Client()
+
+trigger = '!'
+database_name = 'Test.db'
+
+
+async def run_commands(message):
+    """Checks if message is a command and calls command method if it is
+
+    :param message:
+    :return:
+    """
+    commands = {'info': send_data}
+    for key in commands:
+        if message.content.startswith(trigger + key):
+            await commands[key](message)
+
+
+async def send_data(message):
+    """Retrieves data from a database and sends it as a reply
+
+    :param message:
+    :return:
+    """
+    command = message.content.split()
+    if len(command) == 4:
+        data = retrieve_data(database_name, command[1], command[2], command[3])
+        await message.channel.send(data)
+    else:
+        await message.channel.send('!info [Table Name] [Search Col] [Search Parameter]')
 
 
 @client.event
@@ -26,8 +56,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    await run_commands(message)
 
 
 client.run('your token here')
